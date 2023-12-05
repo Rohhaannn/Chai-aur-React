520678d054e7a24234ec34112da1f0cb75642abe
Rohan <shingaderohan96@gmail.com>
2023-12-05 18:24:55 +0530

    Library & JSX - 3 & 4th video

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "Chai Aur Code"
  
   return (
     <>
       <Chai/>
-      <h1> Hello this is running through Vite </h1>
+      <h1> Hello this is running through Vite | {username}</h1>
       <p>test para</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Chai or Coffee ??</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'> Visit Google </a>
+)
+
+const anotherUser = "Chai aur code"
+
+const reactElement =  React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click ME to Visit Google | ',
+  anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <App/>
+  // reactElement
+  // anotherElement
+  reactElement
 )
+ 
