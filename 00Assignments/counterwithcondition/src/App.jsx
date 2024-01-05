5a478d664f197c9c5ed4c9fc95d5a4710666ee55
Rohan <shingaderohan96@gmail.com>
2024-01-05 15:28:33 +0530

    assignment counter with condition complete

--- 00Assignments/counterwithcondition/src/App.jsx
+++ 00Assignments/counterwithcondition/src/App.jsx
@@ -4,17 +4,45 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [counter, setCounter] = useState(15)
   
+
+  // let counter = 5;
+
+  const addValue = () => {
+    // let count = counter + 1;
+    // console.log("value changed", count);
+    // setCounter(counter + 1);
+
+    if(counter >= 20) {
+      alert("You cannot increase value greater than 20");
+    } else {
+      setCounter(counter + 1);
+    }
+    
+  }
+
+
+  const removeValue = () => {
+    // setCounter(counter - 1);
+    if(counter <= 0) {
+      alert("You cannot decrease value less than 0");
+    } else {
+      setCounter(counter - 1);
+    }
+  }
+
 
   return (
     <>
+
       <h1> Chai aur Code </h1>
-      <h2> Counter Value : 5 </h2>
+      <h2> Counter Value : {counter} </h2>
 
-      let counter : 5
+      <button onClick={addValue}> Add Value </button>
+      <button onClick={removeValue}> remove Value </button>
 
-      <button> Add Value </button>
-      <button> remove Value </button>
     </>
   )
 }
