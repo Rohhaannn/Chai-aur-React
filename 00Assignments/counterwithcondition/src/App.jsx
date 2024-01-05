9587c1c2363ec794f778a68315b96dda73634809
Rohan <shingaderohan96@gmail.com>
2024-01-05 15:08:48 +0530

    assignment project added

---
+++ 00Assignments/counterwithcondition/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1> Chai aur Code </h1>
+      <h2> Counter Value : 5 </h2>
+
+      let counter : 5
+
+      <button> Add Value </button>
+      <button> remove Value </button>
+    </>
+  )
+}
+
+export default App
