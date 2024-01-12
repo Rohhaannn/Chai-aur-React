e44b5b158338038a85e9e38b8f53d5e0768e34a9
Rohan <shingaderohan96@gmail.com>
2024-01-12 10:20:31 +0530

    indentation

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -39,8 +39,7 @@ function App() {
           className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat text-black"
           style={{
               backgroundImage: `url('https://images.pexels.com/photos/1076885/pexels-photo-1076885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-          }}
-        >
+          }}>
 
           <div className="w-full">
             <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
