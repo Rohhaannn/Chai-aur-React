3aceb019c17a6b28b32c94a454cd646f9fcc758d
Rohan <shingaderohan96@gmail.com>
2024-01-13 22:43:26 +0530

    Video 12: React-Router completed

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+// import { useState, useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+
+function Github() {
+  // const [data, setData] = useState([])
+  // useEffect(() => {
+  //   fetch('https://api.github.com/users/Rohan-Shingade')
+  //     .then(res =>res.json())
+  //     .then(data => {
+  //       console.log(data);
+  //       setData(data)
+  //     })
+  // }, [])
+
+  //after writing 'githubInfoLoader' method below we commented above useEffect hook which stopped reeceving data below (data.followers). So we are importing a hook called useLoaderData an writing it here.
+
+  const data = useLoaderData()
+  
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github Followers : {data.followers}
+      <img src={data.avatar_url} alt="Git Profile Pic" width={280}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/Rohan-Shingade')
+  return response.json()
+}
