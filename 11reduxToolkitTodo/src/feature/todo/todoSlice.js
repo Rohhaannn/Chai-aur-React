6252b2f91bb4859e2fa67417bdb4ca4c4ac8df01
Rohan <shingaderohan96@gmail.com>
2024-01-22 16:26:34 +0530

    creating reducers

--- 11reduxToolkitTodo/src/feature/todo/todoSlice.js
+++ 11reduxToolkitTodo/src/feature/todo/todoSlice.js
@@ -9,6 +9,17 @@ export const todoSlice = createSlice({
   name:'todo',
   initialState,
   reducers: {
-    addTodo: sayHello
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload
+      }
+      state.todos.push(todo)
+    },
+
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
+    },
+
   }
 })
