6ec607ff9745ddbca5e342e5bbb8059ebe6c4c8b
Rohan <shingaderohan96@gmail.com>
2024-01-26 20:05:03 +0530

    indentation adjustments

--- 11reduxToolkitTodo/src/components/AddTodo.jsx
+++ 11reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import {addTodo} from '../feature/todo/todoSlice'
+import { addTodo } from '../feature/todo/todoSlice'
 
 function AddTodo() {
 
