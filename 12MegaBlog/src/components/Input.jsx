25af18616d8f0410e99656848ff8ad8e22e96092
Rohan <shingaderohan96@gmail.com>
2024-02-09 18:45:39 +0530

    indentation

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React,{useId} from 'react'
+import React,{ useId } from 'react'
 
 const Input = React.forwardRef( function Input({
   label,
@@ -23,7 +23,7 @@ const Input = React.forwardRef( function Input({
         ref={ref}
         {...props}
         id={id}
-        />
+      />
     </div>
   )
 })
