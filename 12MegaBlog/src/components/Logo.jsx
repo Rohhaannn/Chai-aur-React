9f5b930dfec2834295d195bb7e2c3791ff240bec
Rohan <shingaderohan96@gmail.com>
2024-03-18 17:39:48 +0530

    Logo Updated

--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import post from '../assets/post.png'
 
 function Logo({width = "100px"}) {
   return (
     <div className='text-center'>
-      <img src="https://cdn-icons-png.flaticon.com/512/1809/1809216.png" width={50} height={50} alt="React Post Logo" />
+      {/* <img src="https://cdn-icons-png.flaticon.com/512/1809/1809216.png" width={50} height={50} alt="React Post Logo" /> */}
+      <img src={post} width={50} height={50} alt="React Post Logo" />
     </div>
   )
 }
